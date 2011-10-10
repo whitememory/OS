99c14411b741bab05aab353812b24de5423fefe2
prudentiae <prudentiae@kaist.ac.kr>
2011-10-10 16:24:57 -0700

    lessfd function 만들기, sysopen prototype

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,14 +5,23 @@
 #include "threads/thread.h"
 #include "threads/synch.h"
 
+bool less_fd(const struct list_elem *e1, const struct list_elem *e2, void *aux); 
 static void syscall_handler (struct intr_frame *);
 static int syswrite(int fd, const char *buf, unsigned length);
+static int sysopen(const char *fname);
 
 static struct lock mutex;
+struct list fdList;
+struct fileDesc {
+  struct file *f;
+  int fd;
+  struct list_elem elem;
+};
 
 void
 syscall_init (void) 
 {
+  list_init(&fdList);
   lock_init(&mutex);
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
@@ -26,7 +35,7 @@ syscall_handler (struct intr_frame *f UNUSED)
   uint32_t number = *(uint32_t *)esp;
   switch(number){
     case SYS_OPEN:
-      
+      sysopen((char*)*argv);
     case SYS_WRITE:
       syswrite((int)*argv, (char*)*(argv+1), *(argv+2));
       break;
@@ -54,6 +63,30 @@ syswrite(int fd, const char *buf, unsigned length){
 }
 
 static int
-sysopen(char *fname){
+sysopen(const char *fname){
+  struct file *f;
+  struct fileDesc *fDesc = (struct fileDesc*)malloc(sizeof(struct fileDesc));
+  f = filesys_open(fname);
+  if(f==NULL){
+    free(fDesc);
+    return -1;
+  }
+  fDesc->file = f;
+  fDesc->fd = k;
+}
+
+int
+getSmallestFd(){
+  
   return -1;
+}
+
+
+
+
+bool
+less_fd(const struct list_elem *e1, const struct list_elem *e2, void *aux){
+  struct fileDesc *f1 = list_entry(e1, struct fileDesc, elem);
+  struct fileDesc *f2 = list_entry(e2, struct fileDesc, elem);
+  return f1->fd < f2->fd;
 }
